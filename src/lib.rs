455580bcf8dcacf7d3bacf6e2472a2fdb0833d2c
a.navrotskiy <navrotskiy@corp.mail.ru>
2016-07-12 16:03:55 +0300

    Add wrapper for WideCharToMultiByte

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,32 @@
+#[cfg(windows)]
 pub mod windows;
-// pub fn oem_to_string(&[u8]) -> Result<String>
+#[cfg(windows)]
+pub use windows::{oem_to_string, ansi_to_string, string_to_oem, string_to_ansi};
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use std::io::Result;
     #[test]
-    fn it_works() {}
+    fn oem_to_string_test() {
+        to_string_test(oem_to_string);
+    }
+    #[test]
+    fn ansi_to_string_test() {
+        to_string_test(ansi_to_string);
+    }
+    #[test]
+    fn string_to_oem_test() {
+        from_string_test(string_to_oem);
+    }
+    #[test]
+    fn string_to_ansi_test() {
+        from_string_test(string_to_ansi);
+    }
+    fn to_string_test<F: FnOnce(&[u8]) -> Result<String>>(f: F) {
+        assert_eq!(f(b"Test").unwrap(), "Test");
+    }
+    fn from_string_test<F: FnOnce(&str) -> Result<Vec<u8>>>(f: F) {
+        assert_eq!(&f("Test").unwrap(), b"Test");
+    }
 }
--- src/windows.rs
+++ src/windows.rs
@@ -3,7 +3,9 @@ extern crate kernel32;
 
 use std::ptr;
 use std::io::{Error, ErrorKind, Result};
-use self::winapi::DWORD;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+use self::winapi::{BOOL, DWORD};
 
 pub const MB_PRECOMPOSED: DWORD = 0x00000001;
 pub const MB_COMPOSITE: DWORD = 0x00000002;
@@ -15,6 +17,40 @@ pub const WC_SEPCHARS: DWORD = 0x00000020;
 pub const WC_DEFAULTCHAR: DWORD = 0x00000040;
 pub const WC_ERR_INVALID_CHARS: DWORD = 0x00000080;
 pub const WC_NO_BEST_FIT_CHARS: DWORD = 0x00000400;
+
+pub fn oem_to_string(data: &[u8]) -> Result<String> {
+    multi_byte_to_wide_char(winapi::CP_OEMCP, MB_ERR_INVALID_CHARS, data)
+}
+
+pub fn ansi_to_string(data: &[u8]) -> Result<String> {
+    multi_byte_to_wide_char(winapi::CP_ACP, MB_ERR_INVALID_CHARS, data)
+}
+
+pub fn string_to_multibyte(codepage: DWORD,
+                           data: &str,
+                           default_char: Option<u8>)
+                           -> Result<Vec<u8>> {
+    let wstr: Vec<u16> = OsStr::new(data).encode_wide().collect();
+    wide_char_to_multi_byte(codepage,
+                            WC_COMPOSITECHECK,
+                            &wstr,
+                            default_char,
+                            default_char.is_none())
+        .and_then(|(data, invalid)| if invalid {
+            Err(Error::new(ErrorKind::InvalidInput,
+                           "Can't convert some characters to multibyte charset"))
+        } else {
+            Ok(data)
+        })
+}
+
+pub fn string_to_oem(data: &str) -> Result<Vec<u8>> {
+    string_to_multibyte(winapi::CP_OEMCP, data, None)
+}
+
+pub fn string_to_ansi(data: &str) -> Result<Vec<u8>> {
+    string_to_multibyte(winapi::CP_ACP, data, None)
+}
 
 pub fn multi_byte_to_wide_char(codepage: DWORD,
                                flags: DWORD,
@@ -52,6 +88,67 @@ pub fn multi_byte_to_wide_char(codepage: DWORD,
 
 }
 
+pub fn wide_char_to_multi_byte(codepage: DWORD,
+                               flags: DWORD,
+                               wide_char_str: &[u16],
+                               default_char: Option<u8>,
+                               use_default_char_flag: bool)
+                               -> Result<(Vec<u8>, bool)> {
+    // Empty string
+    if wide_char_str.len() == 0 {
+        return Ok((Vec::new(), false));
+    }
+    unsafe {
+        // Get length of multibyte string
+        let len = kernel32::WideCharToMultiByte(codepage,
+                                                flags,
+                                                wide_char_str.as_ptr(),
+                                                wide_char_str.len() as i32,
+                                                ptr::null_mut(),
+                                                0,
+                                                ptr::null(),
+                                                ptr::null_mut());
+
+        if len > 0 {
+            // Convert from UTF-16 to multibyte
+            let mut astr: Vec<u8> = Vec::with_capacity(len as usize);
+            astr.set_len(len as usize);
+            let default_char_ref: [i8; 1] = match default_char {
+                Some(c) => [c as i8],
+                None => [0],
+            };
+            let mut use_char_ref: [BOOL; 1] = [0];
+            let len = kernel32::WideCharToMultiByte(codepage,
+                                                    flags,
+                                                    wide_char_str.as_ptr(),
+                                                    wide_char_str.len() as i32,
+                                                    astr.as_mut_ptr() as winapi::LPSTR,
+                                                    len,
+                                                    match default_char {
+                                                        Some(_) => default_char_ref.as_ptr(),
+                                                        None => ptr::null(),
+                                                    },
+                                                    match use_default_char_flag {
+                                                        true => use_char_ref.as_mut_ptr(),
+                                                        false => ptr::null_mut(),
+                                                    });
+            if (len as usize) == astr.len() {
+                return Ok((astr, use_char_ref[0] != 0));
+            }
+            if len > 0 {
+                return Ok((astr[0..(len as usize)].to_vec(), use_char_ref[0] != 0));
+            }
+        }
+        Err(Error::last_os_error())
+    }
+}
+
+#[test]
+fn multi_byte_to_wide_char_empty() {
+    assert_eq!(multi_byte_to_wide_char(winapi::CP_ACP, MB_ERR_INVALID_CHARS, b"").unwrap(),
+               "");
+}
+
 #[test]
 fn multi_byte_to_wide_char_ascii() {
     assert_eq!(multi_byte_to_wide_char(winapi::CP_ACP, MB_ERR_INVALID_CHARS, b"Test").unwrap(),
@@ -70,4 +167,72 @@ fn multi_byte_to_wide_char_utf8() {
 #[test]
 fn multi_byte_to_wide_char_invalid() {
     assert!(multi_byte_to_wide_char(winapi::CP_UTF8, MB_ERR_INVALID_CHARS, b"Test\xC0").is_err());
+}
+
+#[test]
+fn wide_char_to_multi_byte_empty() {
+    assert_eq!(wide_char_to_multi_byte(winapi::CP_UTF8, WC_ERR_INVALID_CHARS, &[], None, false)
+                   .unwrap(),
+               (b"".to_vec(), false));
+}
+
+#[test]
+fn wide_char_to_multi_byte_ascii() {
+    assert_eq!(wide_char_to_multi_byte(winapi::CP_ACP,
+                                       WC_COMPOSITECHECK,
+                                       &[0x0054, 0x0065, 0x0073, 0x0074],
+                                       None,
+                                       true)
+                   .unwrap(),
+               (b"Test".to_vec(), false));
+}
+
+#[test]
+fn wide_char_to_multi_byte_utf8() {
+    assert_eq!(wide_char_to_multi_byte(winapi::CP_UTF8,
+                                       WC_ERR_INVALID_CHARS,
+                                       &[0x6F22],
+                                       None,
+                                       false)
+                   .unwrap(),
+               (b"\xE6\xBC\xA2".to_vec(), false));
+}
+
+#[test]
+fn wide_char_to_multi_byte_replace() {
+    assert_eq!(wide_char_to_multi_byte(winapi::CP_ACP,
+                                       WC_DEFAULTCHAR | WC_COMPOSITECHECK,
+                                       &[0x0054, 0x0065, 0x0073, 0x0074, 0x6F22, 0x0029],
+                                       Some(b':'),
+                                       true)
+                   .unwrap(),
+               (b"Test:)".to_vec(), true));
+}
+
+#[test]
+fn wide_char_to_multi_byte_invalid() {
+    assert_eq!(wide_char_to_multi_byte(winapi::CP_ACP,
+                                       WC_COMPOSITECHECK,
+                                       &[0x6F22],
+                                       Some(b':'),
+                                       true)
+                   .unwrap(),
+               (b":".to_vec(), true));
+    assert_eq!(wide_char_to_multi_byte(winapi::CP_ACP,
+                                       WC_COMPOSITECHECK,
+                                       &[0x0020],
+                                       Some(b':'),
+                                       true)
+                   .unwrap(),
+               (b" ".to_vec(), false));
+}
+
+#[test]
+fn oem_to_string_test() {
+    assert_eq!(oem_to_string(b"Test").unwrap(), "Test");
+}
+
+#[test]
+fn ansi_to_string_test() {
+    assert_eq!(ansi_to_string(b"Test").unwrap(), "Test");
 }
